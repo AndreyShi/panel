dfc2438194a61619011d73c7bd53e7a772d9d843
a.shiliaev <andrey.shilyayev.89@mail.ru>
2025-08-14 15:32:54 +0300

    upd

--- dashboard_arrow_test.py
+++ dashboard_arrow_test.py
@@ -1,12 +1,16 @@
 import pygame
 import sys
+import time
 
 def scale_image(image, factor):
     """Масштабирует изображение с сохранением пропорций"""
     new_size = (int(image.get_width() * factor), 
                 int(image.get_height() * factor))
     return pygame.transform.scale(image, new_size)
-
+# Цвета
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
 # Настройки окна
 WIDTH, HEIGHT = 1607, 906
 # Инициализация
@@ -27,35 +31,58 @@ except:
 try:
     needle_img = pygame.image.load("needle.png").convert_alpha()  # Важно!
     # Уменьшаем в 0.5 раза (50% от оригинала)
-    needle_img = scale_image(needle_img, 0.5)
-    needle_rect = needle_img.get_rect(center=(460, 380))
+    needle_img = scale_image(needle_img, 0.51)
+    needle_rect = needle_img.get_rect(center=(463, 383))
 except:
     print("Ошибка: needle.png не найден. Создаю красный прямоугольник.")
     needle_img = pygame.Surface((20, 150), pygame.SRCALPHA)
     pygame.draw.rect(needle_img, (255, 0, 0), (0, 0, 20, 150))
     needle_rect = needle_img.get_rect(center=(450, 240))
 
-angle = 295  # Начальный угол
+angle = 0  # Начальный угол
 
 # Основной цикл
 clock = pygame.time.Clock()
 running = True
+ccw = -1
+toup = True
 
+# Добавление текста (скорость, RPM)
+font = pygame.font.SysFont('Arial', 133)
+# Таймеры
+last_update_time = time.time()
+update_interval = 0.25  # Обновлять скорость каждые 0.5 секунды
+speed_text = font.render(f"{int(angle*0.73)}", True, WHITE)
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     # Обновление угла (имитация данных)
-    #angle = (angle + 1) % 360
+    if angle < 109 and toup == True:
+        angle = (angle + 1) % 110
+    elif angle == 0:
+        toup = True
+    else:
+        angle = (angle - 1) % 110
+        toup = False
 
     # Отрисовка
     screen.blit(background, (0, 0))
 
     # Вращение стрелки
-    rotated_needle = pygame.transform.rotate(needle_img, -angle)  # Минус для правильного направления
+    rotated_needle = pygame.transform.rotate(needle_img, ccw * (angle + 228))  # Минус для правильного направления
     new_rect = rotated_needle.get_rect(center=needle_rect.center)
     screen.blit(rotated_needle, new_rect.topleft)
+
+        # --- Обновление данных (реже, чем кадры) ---
+    current_time = time.time()
+    if current_time - last_update_time > update_interval:
+        last_update_time = current_time
+        # Обновление скорости
+        speed_text = font.render(f"{int(angle*0.73)}", True, WHITE)
+        #speed_text = font.render(f"{int(43)}", True, WHITE)
+    screen.blit(speed_text, (730, 35))
 
     pygame.display.flip()
     clock.tick(60)  # 60 FPS
