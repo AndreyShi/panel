1b9b60e1a8d09f83b83df96a937ca52b1694fe24
a.shiliaev <andrey.shilyayev.89@mail.ru>
2025-09-10 14:33:28 +0300

    add discover_device

--- bt_linux.py
+++ bt_linux.py
@@ -4,15 +4,28 @@ import subprocess
 import time
 import os
 
+def discover_devices():
+    print("Поиск Bluetooth устройств...")
+    devices = bluetooth.discover_devices(lookup_names=True)
+            
+    for addr, name in devices:
+        print(f"Найдено устройство: {name} - {addr}")
+        if "ELM327" in name.upper() or "OBD" in name.upper():
+            print(f"Найден ELM327: {name}")
+            return addr
+    return None
+
 def main():
     # 1. Подключаем Bluetooth socket
-    sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-    device_address = "00:1D:A5:06:04:CB"  # Ваш MAC адрес
-    sock.connect((device_address, 1))
-    print("Bluetooth socket подключен")
+    #sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM) 
+    #device_address = "00:1D:A5:06:04:CB"  # Ваш MAC адрес
+    device_address = discover_devices()
+    #sock.connect((device_address, 1))
+    #print("Bluetooth socket подключен")
 
     print("Создаем RFCOMM порт...")
     subprocess.run(['sudo', 'rfcomm', 'release', 'all'], check=False)
+    #эквивалентно sudo rfcomm bind /dev/rfcomm0 XX:XX:XX:XX:XX:XX 1
     subprocess.run(['sudo', 'rfcomm', 'bind', '/dev/rfcomm0', device_address, '1'], check=True)
     time.sleep(2)
         
